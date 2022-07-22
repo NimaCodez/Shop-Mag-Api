7bab7d8b590c3e3e9246e6cbcd42ade89f5bf6da
Nima <nimarahmati8par@gmail.com>
2022-07-23 00:31:51 +0430

    Courses routes configured - Get All courses done

--- app/routes/admin/admin.routes.js
+++ app/routes/admin/admin.routes.js
@@ -1,5 +1,6 @@
 const { AdminBlogsRouter } = require('./blog.router');
 const { AdminCategoryRouter } = require('./category.router');
+const { AdminCourseRouter } = require('./course.router');
 const { AdminProductsRouter } = require('./product.router');
 const adminRouter = require('express').Router();
 
@@ -8,6 +9,8 @@ const adminRouter = require('express').Router();
  *  tags:
  *      -   name: Admin-Panel
  *          description: Admin ROUTES With pro Accessibility
+ *      -   name: Course(Admin-Panel)
+ *          description: Admin routes of courses for management.
  *      -   name: Product(Admin-Panel)
  *          description: Admin Rights for products
  *      -   name: Category(Admin-Panel)
@@ -16,8 +19,9 @@ const adminRouter = require('express').Router();
  *          description: Admin Panel Rights and routes for Blogs
  */
 adminRouter.use('/category', AdminCategoryRouter)
-adminRouter.use('/products', AdminProductsRouter)
 adminRouter.use('/blogs', AdminBlogsRouter)
+adminRouter.use('/products', AdminProductsRouter)
+adminRouter.use('/courses', AdminCourseRouter)
 
 module.exports = {
     adminRouter
