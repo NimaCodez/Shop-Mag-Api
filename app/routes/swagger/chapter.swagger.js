1c743db1d4ab0d45f1d7c477e98ab0527e4a825b
Nima <nimarahmati8par@gmail.com>
2022-07-25 21:29:11 +0430

    Chapter controller updated

--- app/routes/swagger/chapter.swagger.js
+++ app/routes/swagger/chapter.swagger.js
@@ -21,6 +21,34 @@
 
 /**
  * @swagger
+ *  definitions:
+ *      ChaptersOfCourseDefinition:
+ *          type: object
+ *          properties:
+ *              status:
+ *                  type: integer
+ *                  example: 20x, 40x, 500
+ *              data:
+ *                  type: object
+ *                  properties:
+ *                      course:
+ *                          type: object
+ *                          properties:
+ *                              _id:
+ *                                  type: string
+ *                                  example: 6279e994c1e47a98d0f356d3
+ *                              title:
+ *                                  type: string
+ *                                  exmaple: Course's title
+ *                              chapters:
+ *                                  type: array
+ *                                  items:
+ *                                      type: object
+ *                                  example: [{_id: '6279e994c1e47a98d0f356d3', title: "chapter's title", text: "chapter's text"}]
+ */
+
+/**
+ * @swagger
  *  /admin/chapter/add:
  *      put:
  *          tags: [Chapter(Admin-Panel)]
@@ -33,7 +61,7 @@
  *                          $ref: '#/components/schemas/AddChapter'
  *                  application/json:
  *                      schema:
- *                          $ref: '#/components/AddChapter'
+ *                          $ref: '#/components/schemas/AddChapter'
  *          responses:
  *              201:
  *                  description: Success
@@ -41,4 +69,24 @@
  *                  description: Bad Request
  *              500:
  *                  description: INTERNAL SERVER ERROR 
+ */
+
+/**
+ * @swagger
+ *  /admin/chapter/list/{chapterID}:
+ *      get:
+ *          tags: [Chapter(Admin-Panel)]
+ *          summary: Create new chapter four a course
+ *          parameters:
+ *              -   in: path
+ *                  name: chapterID
+ *                  type: string
+ *                  required: true
+ *          responses:
+ *              200:
+ *                  description: SUCCESS
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/definitions/ChaptersOfCourseDefinition'
  */
