b7e58e4cebf38f330a5c071ac96dbcb1fc20aac3
Nima <nimarahmati8par@gmail.com>
2022-08-06 17:52:22 +0430

    Configurations and Make Database Online

--- app/http/controllers/user/auth/auth.controller.js
+++ app/http/controllers/user/auth/auth.controller.js
@@ -9,7 +9,7 @@ const Controller = require('../../controller');
 class UserAuthController extends Controller {
     async GetOtp(req, res, next) {
         try {
-            await GetOtpSchema.validateAsync(req.body);
+            // await GetOtpSchema.validateAsync(req.body);
             const { mobile } = req.body;
             const code = GenerateRandomNumber()
             const result = await this.saveUser(mobile, code);
@@ -27,7 +27,7 @@ class UserAuthController extends Controller {
     }
     async CheckOtp(req, res, next) {
         try {
-            await CheckOtpSchema.validateAsync(req.body);
+            // await CheckOtpSchema.validateAsync(req.body);
             const { mobile, code } = req.body;
             const user = await UserModel.findOne({mobile});
             if(!user) throw createError.NotFound("Your Account was not Found 🔍");
