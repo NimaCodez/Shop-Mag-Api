59a42b300783d966f4adc93a8b9a9da046a68e0f
Nima <nimarahmati8par@gmail.com>
2022-08-10 21:04:11 +0430

    RBAC started - files created- routers and Controllers and swagger files created

---
+++ app/http/controllers/RBAC/permissions.controller.js
@@ -0,0 +1,9 @@
+const Controller = require("../controller");
+
+class PermissionsController extends Controller {
+    
+}
+
+module.exports = {
+    PermissionsController: new PermissionsController(),
+}
