2bcb51a71a6f26f0f207ba269fa8f084320c79c5
Nima <nimarahmati8par@gmail.com>
2022-08-30 16:02:25 +0430

    Blog Model's Comment Schema Updated

--- app/models/public.schema.js
+++ app/models/public.schema.js
@@ -4,7 +4,8 @@ const CommentSchema = new Schema({
     user: { type: Types.ObjectId, ref: "user", required: true },
     comment: { type: String, required: true},
     createdAt: { type: Date, default: Date.now()},
-    parent: { type: Types.ObjectId, ref: "comment" }
+    parent: { type: Types.ObjectId, ref: "comment" },
+    openToComment: { type: Boolean, required: true, default: true }
 }, {
     timestamps: {
         createdAt: true
